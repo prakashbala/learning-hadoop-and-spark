af634817712bfb1972b770ffb73707f21b8970c3
Prakash B <prakashb@pop-os.localdomain>
2022-02-07 14:27:47 +0530

    Created cluster

--- 0a-Setup-Hadoop/GCP-Dataproc/0_setup-dataproc.sh
+++ 0a-Setup-Hadoop/GCP-Dataproc/0_setup-dataproc.sh
@@ -5,19 +5,5 @@
 # --zone <your-zone>
 # --project <your-project>
 
-gcloud beta dataproc clusters create demo 
-    --enable-component-gateway 
-    --region us-central1 
-    --subnet default 
-    --zone us-central1-a 
-    --master-machine-type n1-standard-4 
-    --master-boot-disk-size 500 
-    --num-workers 2 
-    --worker-machine-type n1-standard-2 
-    --worker-boot-disk-size 500 
-    --image-version 1.3-deb9 
-    --optional-components ANACONDA,HIVE_WEBHCAT,JUPYTER,DRUID,PRESTO,ZOOKEEPER 
-    --project nosql-langit
+gcloud dataproc clusters create my-demo --enable-component-gateway --region us-central1 --zone us-central1-a --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 500 --image-version 2.0-debian10 --optional-components HIVE_WEBHCAT,JUPYTER,DRUID,PRESTO,ZOOKEEPER --project seismic-honor-340416
 
-# run as one line
-gcloud beta dataproc clusters create demo --enable-component-gateway --region us-central1 --subnet default --zone us-central1-a --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 500 --image-version 1.3-deb9 --optional-components ANACONDA,HIVE_WEBHCAT,JUPYTER,DRUID,PRESTO,ZOOKEEPER --project nosql-langit
